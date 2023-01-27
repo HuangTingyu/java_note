98ec69c20659823326ee7e8493b0d622f98a5314
sakurahuang <sakurahuang@tencent.com>
2023-01-27 17:44:30 +0800

    EventQueue开启线程

--- 环境安装/algorithm/src/Main.java
+++ 环境安装/algorithm/src/Main.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-        JFrame frame = new JFrame("hello");//hello为窗口标题
-        frame.setSize(500,500);//窗口大小
-        frame.setResizable(false);//禁止调节窗口大小
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭窗口的时候结束运行程序
-        frame.setVisible(true);
+//        EventQueue开启一个新线程
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("hello");//hello为窗口标题
+            frame.setSize(500,500);//窗口大小
+            frame.setResizable(false);//禁止调节窗口大小
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭窗口的时候结束运行程序
+            frame.setVisible(true);
+        });
+
     }
 }
